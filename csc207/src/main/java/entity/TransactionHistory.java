14d531740ae200254b2945062c3db1a4592d303f
rachelcheng <rachelct.cheng@mail.utoronto.ca>
2024-11-09 16:10:32 -0500

    Added javadoc to methods and classes in TransactionHistory.java

--- csc207/src/main/java/entity/TransactionHistory.java
+++ csc207/src/main/java/entity/TransactionHistory.java
@@ -3,29 +3,49 @@ package entity;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * Stores the Transaction history for app.
+ */
 public class TransactionHistory {
-    public ArrayList<Transaction> history;
+    private ArrayList<Transaction> history;
 
     public TransactionHistory() {
-        history = new ArrayList<Transaction>();
+        history = new ArrayList<>();
     }
 
+    /**
+     * Returns total amount spent??? net balance.
+     * @return total amount
+     */
     public int getAmountTotal() {
         int sum = 0;
-        for(var t : history)
-            sum += t.amount;
+        for (var t : history) {
+            sum += t.getAmount();
+        }
         return sum;
     }
 
+    /**
+     * Returns net amount between start and stop dates.
+     * @param start the start date (inclusive)
+     * @param stop the end date (inclusive)
+     * @return net amount.
+     */
     public int getAmountBetween(Date start, Date stop) {
         int sum = 0;
-        for(var t : history)
-            if(t.date.after(start) && t.date.before(stop))
-                sum += t.amount;
+        for (var t : history) {
+            if (t.getDate().after(start) && t.getDate().before(stop)) {
+                sum += t.getAmount();
+            }
+        }
         return sum;
     }
 
-    public void add(Transaction t) {
-        history.add(t);
+    /**
+     * Adds a transaction into history.
+     * @param transaction the transaction to be added.
+     */
+    public void add(Transaction transaction) {
+        history.add(transaction);
     }
 }
