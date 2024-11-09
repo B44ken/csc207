3a320e89605616635f7341f431978f7aaf60983e
rachelcheng <rachelct.cheng@mail.utoronto.ca>
2024-11-09 15:57:58 -0500

    Tentatively completed AddIncomeViewModel.java

--- csc207/src/main/java/interface_adapter/add_income/AddIncomeViewModel.java
+++ csc207/src/main/java/interface_adapter/add_income/AddIncomeViewModel.java
@@ -3,7 +3,7 @@ package interface_adapter.add_income;
 import interface_adapter.ViewModel;
 
 /**
- * The View Model for Logged In View
+ * The View Model for Add Income View.
  */
 public class AddIncomeViewModel extends ViewModel<AddIncomeState> {
     public AddIncomeViewModel() {
