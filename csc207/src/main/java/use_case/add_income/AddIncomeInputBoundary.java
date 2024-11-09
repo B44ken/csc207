200b064c8f3d0f447724b85d09de922067a0a17b
rachelcheng <rachelct.cheng@mail.utoronto.ca>
2024-11-09 14:20:56 -0500

    Tentatively completed AddIncomeInputBoundary.java

---
+++ csc207/src/main/java/use_case/add_income/AddIncomeInputBoundary.java
@@ -0,0 +1,14 @@
+package use_case.add_income;
+
+/**
+ * Add Income Use Case.
+ */
+
+public interface AddIncomeInputBoundary {
+    /**
+     * Execute the Change Password Use Case.
+     * @param addIncomeInputData the input data for this use case
+     */
+    void execute(AddIncomeInputData addIncomeInputData);
+
+}
