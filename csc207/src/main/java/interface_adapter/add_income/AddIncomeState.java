38495f5786daa365453c789df9e62bfa8e9dde8a
rachelcheng <rachelct.cheng@mail.utoronto.ca>
2024-11-09 15:58:51 -0500

    Tentatively completed AddIncomeState.java

--- csc207/src/main/java/interface_adapter/add_income/AddIncomeState.java
+++ csc207/src/main/java/interface_adapter/add_income/AddIncomeState.java
@@ -1,20 +1,16 @@
 package interface_adapter.add_income;
 
-import java.util.Date;
-
 /**
  * The State information representing the new income transaction to be logged.
  */
 public class AddIncomeState {
     private String name = "";
-    private int amount;
 
-    private Date date;
+    private String addFail;
 
     public AddIncomeState(AddIncomeState copy) {
         this.name = copy.name;
-        this.amount = copy.amount;
-        this.date = copy.date;
+        this.addFail = copy.addFail;
     }
 
     // Because of the previous copy constructor, the default constructor must be explicit.
@@ -25,22 +21,12 @@ public class AddIncomeState {
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
-    public int getAmount() {
-        return amount;
-    }
 
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
+    public void setAddFail(String addFail) {
+        this.addFail = addFail;
     }
 }
