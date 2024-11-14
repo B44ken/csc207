b622f9e7e731f73fd2a180100ae9326b601b2859
serena <serena.ho@mail.utoronto.ca>
2024-11-14 13:12:15 -0500

    added import data

---
+++ csc207/src/main/java/view/GoalView.java
@@ -0,0 +1,16 @@
+package view;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.*;
+
+public class GoalView extends JPanel implements ActionListener, PropertyChangeListener {
+    private String addGoal = "Add Goal";
+
+
+
+}
