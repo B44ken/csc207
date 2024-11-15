7ea714b86475c1d70bc0c8bce9d0abc887f9656d
abadceli <celina.abad@mail.utoronto.ca>
2024-11-15 02:05:00 -0500

    created addbudgetview

---
+++ csc207/src/main/java/view/AddBudgetView.java
@@ -0,0 +1,83 @@
+import java.util.Date;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+import interface_adapter.add_budget.AddBudgetController;
+import interface_adapter.add_budget.AddBudgetState;
+import interface_adapter.add_budget.AddBudgetViewModel;
+
+
+public class AddBudgetView extends JPanel implements ActionListener, PropertyChangeListener {
+
+    private final AddBudgetController addBudgetController;
+    private final AddBudgetViewModel addBudgetViewModel;
+    // change above to what controller is actually called later
+    // need to move the input fields out here unfortunately!!!!!!!!!!!
+
+    public AddBudgetView(AddBudgetViewModel budgetViewModel, AddBudgetController controller) {
+
+        this.addBudgetController = controller;
+        this.addBudgetViewModel = budgetViewModel;
+        addBudgetViewModel.addPropertyChangeListener(this);
+
+        final JLabel title = new JLabel("Add Budget");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JPanel categoryNamePanel = new JPanel();
+        JTextField categoryNameTextField = new JTextField(15);
+        categoryNamePanel.add(new JLabel("Category:"));
+        categoryNamePanel.add(categoryNameTextField);
+
+        JPanel amountPanel = new JPanel();
+        JTextField amountTextField = new JTextField(15);
+        amountPanel.add(new JLabel("Amount:"));
+        amountPanel.add(amountTextField);
+
+        JPanel confirmPanel = new JPanel();
+        JButton confirmButton = new JButton("Confirm");
+        confirmPanel.add(confirmButton);
+
+        JPanel cancelPanel = new JPanel();
+        JButton cancelButton = new JButton("Cancel");
+        cancelPanel.add(cancelButton);
+
+        confirmButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String categoryName = categoryNameTextField.getText();
+                Double amount = Double.valueOf(amountTextField.getText());
+                // input into budget text file here
+                // after everything funnelled into txt file go back to budget view:
+                // addBudgetController.switchToBudgetView();
+            }
+        });
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.add(categoryNamePanel);
+        mainPanel.add(amountPanel);
+
+        JFrame frame = new JFrame("Add Budget");
+        frame.setContentPane(mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JOptionPane.showMessageDialog(this, "Action Performed not implemented yet.");
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        JOptionPane.showMessageDialog(this, "Property Change not implemented yet.");
+    }
+}
