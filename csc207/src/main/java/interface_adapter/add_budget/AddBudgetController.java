9bffb2c5b84603fb57cbb5430a4eed5d26079cee
abadceli <celina.abad@mail.utoronto.ca>
2024-11-15 01:57:35 -0500

    created addbudgetcontroller in interface_adapter

---
+++ csc207/src/main/java/interface_adapter/add_budget/AddBudgetController.java
@@ -0,0 +1,33 @@
+package interface_adapter.add_budget;
+
+import use_case.add_budget.AddBudgetInputBoundary;
+import use_case.add_budget.AddBudgetInputData;
+
+
+/**
+ * Controller for Add Budget use case.
+ */
+public class AddBudgetController {
+    private final AddBudgetInputBoundary addBudgetUseCaseInteractor;
+    public AddBudgetController(AddBudgetInputBoundary addBudgetUseCaseInteractor) {
+        this.addBudgetUseCaseInteractor = addBudgetUseCaseInteractor;
+    }
+
+    /**
+     * Executes the Add Budget use case.
+     * @param categoryName to be added
+     * @param amount
+     *
+     */
+    public void execute(String categoryName, Double amount) {
+        final AddBudgetInputData addBudgetInputData = new AddBudgetInputData(categoryName, amount);
+        addBudgetUseCaseInteractor.execute(addBudgetInputData);
+    }
+
+    /**
+     * Executes the "switch to BudgetView" Use Case.
+     */
+    public void switchToBudgetView() {
+        addBudgetUseCaseInteractor.switchToBudgetView();
+    }
+}
