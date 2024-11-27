0b2085445473fd0259b3f90c3d41aceeb90b0fc4
b44ken <bradboratto@gmail.com>
2024-11-27 05:45:12 -0500

    change csv format

--- csc207/src/main/java/data_access/FileAccess.java
+++ csc207/src/main/java/data_access/FileAccess.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.Instant;
 import java.time.LocalDate;
 
 import entity.Expense;
@@ -24,14 +23,29 @@ public class FileAccess {
 
         var lines = fileContent.split("\n");
         for (var line : lines) {
+            if (line.equals(lines[0]))
+                continue;
+
             var parts = line.split(",");
-            var entry = new Income(
-                parts[1],
-                Double.parseDouble(parts[0]),
-                "category",
-                LocalDate.parse(parts[2])
-            );
-            history.add(entry);
+
+            var amount = Double.parseDouble(parts[0]);
+            var name = parts[1];
+            var category = parts[2];
+            var date = LocalDate.parse(parts[3]);
+            var type = parts[4];
+
+            if (type.equals("income")) {
+                var entry = new Income(name, amount, category, date);
+                history.add(entry);
+            } else if (type.equals("expense")) {
+                var entry = new Expense(name, amount, category, date);
+                history.add(entry);
+            } else if (type.equals("transaction")) {
+                var entry = new Transaction(name, amount, category, date);
+                history.add(entry);
+            } else {
+                throw new RuntimeException("Invalid type");
+            }
         }
         return history;
     }
