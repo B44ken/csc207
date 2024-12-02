7ab1bb72e79adc95a5cdb95e5aa5b557f0fe3269
ethancua <ethan.cua@mail.utoronto.ca>
2024-12-02 00:41:47 -0500

    Made some changes to the Add Expense Use Case. Added some prompts and tried to adhere to CA

--- csc207/src/main/java/interface_adapter/add_expense/AddExpenseController.java
+++ csc207/src/main/java/interface_adapter/add_expense/AddExpenseController.java
@@ -6,31 +6,52 @@ import com.labrats.app.ViewNames;
 
 import data_access.UserData;
 import entity.Expense;
+import use_case.add_expense.AddExpenseOutputBoundary;
+import use_case.add_expense.AddExpenseOutputData;
 import view.ViewSwitcher;
 
 public class AddExpenseController {
     private UserData userData;
     private ViewSwitcher viewSwitcher;
 
-    public AddExpenseController(ViewSwitcher vs, UserData ud) {
+    private AddExpenseOutputBoundary presenter;
+
+    public AddExpenseController(ViewSwitcher vs, UserData ud, AddExpenseOutputBoundary addExpenseOutputBoundary) {
         this.userData = ud;
         this.viewSwitcher = vs;
+        this.presenter = addExpenseOutputBoundary;
     }
 
     public void addExpense(String name, String amountStr, String category) {
         var date = LocalDate.now();
 
-        if (name == null || amountStr == null || category == null) {
-            System.out.println("invalid input");
+
+        if (name.equals("")) {
+            presenter.prepareFailView("Name field cannot be blank");
             return;
+        } else if (amountStr.equals("")) {
+            presenter.prepareFailView("Amount field cannot be blank");
+            return;
+        } else if (category.equals("")) {
+            presenter.prepareFailView("Category field cannot be blank");
+        } else {
+            try {Double.valueOf(amountStr); }
+            catch (NumberFormatException e) {
+                presenter.prepareFailView("Invalid amount for parsing.");
+                return;
+            }
         }
         try {
             var amount = Double.parseDouble(amountStr);
             var t = new Expense(name, amount, category, date);
             userData.getHistory().add(t);
             userData.save();
+
+            final AddExpenseOutputData addExpenseOutputData = new AddExpenseOutputData(name);
+            presenter.prepareSuccessView(addExpenseOutputData);
         } catch (NumberFormatException ex) {
             System.out.println("failed to parse amount");
+            presenter.prepareFailView("failed to parse amount");
         }
     }
 
