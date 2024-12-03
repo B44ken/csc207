c43fd2aad11ba5810fea671589b0376caabe2a17
rachelcheng <rachelct.cheng@mail.utoronto.ca>
2024-12-03 16:40:09 -0500

    committing

--- csc207/src/main/java/use_case/get_insight/GetInsightInteractor.java
+++ csc207/src/main/java/use_case/get_insight/GetInsightInteractor.java
@@ -1,20 +1,18 @@
 package use_case.get_insight;
 
-import data_access.UserData;
-import entity.*;
-import interface_adapter.get_insight.GetInsightPresenter;
-import use_case.get_insight.GetInsightInputData;
-
-import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Objects;
 
-public class GetInsightInteractor implements GetInsightInputBoundary {
+import data_access.UserData;
+import entity.*;
+import interface_adapter.get_insight.GetInsightPresenter;
 
-    public UserData userData;
-    public GetInsightPresenter presenter;
+public class GetInsightInteractor implements GetInsightInputBoundary {
+    private final double percentage = 0.75;
+
+    private UserData userData;
+    private GetInsightPresenter presenter;
     // constant: average spending of UOFT student
-    private double AVERAGE_SPENDING = 1000;
 
     public GetInsightInteractor(UserData userData) {
         this.userData = userData;
@@ -30,18 +28,27 @@ public class GetInsightInteractor implements GetInsightInputBoundary {
      *         get the expense out of the input data.
      *         instantiate the `LogoutOutputData`, which needs to contain the correct insight.
      *         tell the presenter to prepare a success view.
-     * @param inputData
+     * @param inputData the inputData created by Controller.
+     * @return ArrayList output Data object to be returned.
      */
     public ArrayList<Deductible> execute(GetInsightInputData inputData) {
-        ArrayList<Deductible> result = new ArrayList<>();
+        final ArrayList<Deductible> result = new ArrayList<>();
         // for every charity category transaction,
         for (Transaction t : inputData.getInputData()) {
             if (Objects.equals(t.getCategory(), "charity") && t instanceof Expense) {
-                Deductible deductible = new Deductible((Expense) t, new Income(t.getName(), t.getAmount() * 0.75, t.getCategory(), t.getDate()));
+                final Deductible deductible = new Deductible((Expense) t, new Income(t.getName(),
+                        t.getAmount() * percentage, t.getCategory(), t.getDate()));
                 result.add(deductible);
             }
         }
         return result;
     }
 
+    public UserData getUserData() {
+        return userData;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
 }
