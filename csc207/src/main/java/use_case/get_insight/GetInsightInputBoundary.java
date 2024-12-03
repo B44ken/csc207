0dd00c64391e81dfdfb93c19de8b85fb450d749c
rachelcheng <rachelct.cheng@mail.utoronto.ca>
2024-12-03 18:47:41 -0500

    fixed interface

--- csc207/src/main/java/use_case/get_insight/GetInsightInputBoundary.java
+++ csc207/src/main/java/use_case/get_insight/GetInsightInputBoundary.java
@@ -1,8 +1,17 @@
 package use_case.get_insight;
 
+import java.util.ArrayList;
+
+import entity.Deductible;
+
 /**
- * Abstraction layer for Get Insight Use Case, according to Dependency Inversion Principle.
+ * ArrayList interface.
  */
 public interface GetInsightInputBoundary {
-    Object execute(GetInsightInputData getInsightInputData);
+    /**
+     * Execute method.
+     * @param getInsightInputData input data.
+     * @return ArrayList type.
+     */
+    ArrayList<Deductible> execute(GetInsightInputData getInsightInputData);
 }
